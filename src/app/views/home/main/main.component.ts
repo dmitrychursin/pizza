8365075c34bd6593e7686ea9119262b01ef2dafe
Dmitry Chursin <dmitrychursin178@gmail.com>
2023-05-11 16:24:29 +0300

    changet popup

--- src/app/views/home/main/main.component.ts
+++ src/app/views/home/main/main.component.ts
@@ -54,7 +54,7 @@ export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
   private popupComponent!: PopupComponent;
 
   ngAfterViewInit() {
-    this.popupComponent.open();
+    // this.popupComponent.open();
   }
 
   ngOnDestroy() {
